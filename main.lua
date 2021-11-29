57bc9106c08bb37d9008ad3c33e75f2257116f5c
Willson Smith <me@willsonsmith.com>
2021-11-28 23:23:04 -0500

    minor change

--- main.lua
+++ main.lua
@@ -11,6 +11,7 @@ function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
   love.window.setMode(800, 600, game)
   love.window.setTitle("ECS Game Engine")
+
   game.world = tiny.world(
     lamp({
       x = love.graphics:getWidth() / 2,
