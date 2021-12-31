9ca66c7f060e95d078761ff8dc8844f50d76d26b
Willson Smith <me@willsonsmith.com>
2021-12-31 14:38:22 -0500

    initial rewrite

--- engine/system/collision/box2d.lua
+++ engine/system/collision/box2d.lua
@@ -1,28 +1,38 @@
 local tiny = require 'lib/tiny'
 local windfield = require 'lib/windfield/windfield'
-local entity_box2dWorld = require "engine/entity/physics/box2d"
+local box2d_world = require "engine/entity/physics/box2d"
 
-local collision_system = {}
-collision_system.colliders = tiny.processingSystem()
-collision_system.colliders.filter = tiny.requireAll('collision')
-function collision_system.colliders:onAdd(e)
-  local box2dWorld
+local box2d_colliders = {}
+
+box2d_colliders.generator = tiny.processingSystem()
+box2d_colliders.generator.filter = tiny.requireAll("collision")
+
+box2d_colliders.world = nil
+
+function box2d_colliders.generator:onAdd(e)
+
+  -- Create the world if it doesn't exist
+  local world = box2d_colliders.world
   for _, e in ipairs(self.world.entities) do
-    if box2dWorld then break end
-    local filter = tiny.requireAll("box2d", "world")
+    if world then break end
+    local filter = tiny.requireAll('box2d', 'world')
     if filter(self.world, e) then
-      box2dWorld = e
+      world = e
       break
     end
   end
-  if not box2dWorld then
-    local entity = entity_box2dWorld(0, 9.8, true)
-    box2dWorld = entity.world
-    tiny.addEntity(self.world, entity)
+
+  if not world then
+    world = box2d_world()
+    self.world:addEntity(world)
   end
 
+  box2d_colliders.world = world
 
+  -- Create the collider
+  local collider = e.collision
+  
 end
 
-function collision_system.colliders:onRemove(e)
+function box2d_colliders.generator:onRemove(e)
 end
