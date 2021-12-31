180664fb14bc4c7db9d1728222e1b4d2fcaf2891
Willson Smith <me@willsonsmith.com>
2021-12-31 14:44:27 -0500

    rough cut

--- engine/system/collision/box2d.lua
+++ engine/system/collision/box2d.lua
@@ -27,12 +27,43 @@ function box2d_colliders.generator:onAdd(e)
     self.world:addEntity(world)
   end
 
-  box2d_colliders.world = world
-
   -- Create the collider
   local collider = e.collision
-  
+  local shape = collider.shape
+  if shape == "polygon" then
+    world:newPolygonCollider(collider.points)
+  end
+
+  for _, collider in ipairs(e.collision) do
+    local shape = collider.shape
+
+    if shape == "rectangle" then
+      -- use the entity's x and y, and the width and height
+      world:newRectangleCollider(e.x, e.y, e.width, e.height)
+    end
+    if shape == "polygon" then
+      world:newPolygonCollider(collider.points)
+    end
+    if shape == "ellipse" then
+      print("ellipse not implemented")
+    end
+  end
+
+  -- Add the world to the box2d_colliders object
+  box2d_colliders.world = world
 end
 
 function box2d_colliders.generator:onRemove(e)
+end
+
+
+box2d_colliders.update = tiny.processingSystem()
+box2d_colliders.update.filter = tiny.requireAll("collision")
+function box2d_colliders.update:process(e, dt)
+  local collider = e.collision
+  if collider.updates_position then
+    local x, y = e.body:getPosition()
+    e.x = x
+    e.y = y
+  end
 end
